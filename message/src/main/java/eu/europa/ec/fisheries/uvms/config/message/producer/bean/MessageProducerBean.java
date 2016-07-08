1ebd4ab54df09e182c8c9cbc0a26f933e2e2175b
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2016-07-08 11:31:01 +0200

    Removed reference to ConfigModel queue

--- message/src/main/java/eu/europa/ec/fisheries/uvms/config/message/producer/bean/MessageProducerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/config/message/producer/bean/MessageProducerBean.java
@@ -35,9 +35,6 @@ public class MessageProducerBean implements MessageProducer {
 
     final static Logger LOG = LoggerFactory.getLogger(MessageProducerBean.class);
 
-    @Resource(mappedName = MessageConstants.QUEUE_DATASOURCE_INTERNAL)
-    private Queue localDbQueue;
-
     @Resource(mappedName = MessageConstants.AUDIT_MODULE_QUEUE)
     private Queue auditQueue;
 
