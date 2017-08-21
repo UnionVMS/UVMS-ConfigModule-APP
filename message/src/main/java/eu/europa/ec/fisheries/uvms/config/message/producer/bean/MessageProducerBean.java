24bf7b44ea5ce89a621d2e6af5fe55d68a143b47
James Sörling <james.p.sorling@consid.se>
2017-08-21 13:30:52 +0200

    jms cleanup

--- message/src/main/java/eu/europa/ec/fisheries/uvms/config/message/producer/bean/MessageProducerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/config/message/producer/bean/MessageProducerBean.java
@@ -12,13 +12,20 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
 package eu.europa.ec.fisheries.uvms.config.message.producer.bean;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
+import javax.ejb.Singleton;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.enterprise.event.Observes;
-import javax.jms.*;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
 
-import eu.europa.ec.fisheries.uvms.message.JMSUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,9 +37,7 @@ import eu.europa.ec.fisheries.uvms.config.message.exception.MessageException;
 import eu.europa.ec.fisheries.uvms.config.message.producer.MessageProducer;
 import eu.europa.ec.fisheries.uvms.config.model.exception.ModelMarshallException;
 import eu.europa.ec.fisheries.uvms.config.model.mapper.JAXBMarshaller;
-import javax.ejb.Singleton;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+import eu.europa.ec.fisheries.uvms.message.JMSUtils;
 
 @Singleton
 public class MessageProducerBean implements MessageProducer {
@@ -46,53 +51,24 @@ public class MessageProducerBean implements MessageProducer {
 
     private ConnectionFactory connectionFactory;
 
-    private Connection connection = null;
-    private Session session = null;
-
     @PostConstruct
     public void init() {
-        InitialContext ctx;
-        try {
-            ctx = new InitialContext();
-        } catch (Exception e) {
-            LOG.error("Failed to get InitialContext",e);
-            throw new RuntimeException(e);
-        }
-        lookupConnectionFactory(ctx);
-        responseQueue = JMSUtils.lookupQueue(ctx, MessageConstants.COMPONENT_RESPONSE_QUEUE);
-        auditQueue = JMSUtils.lookupQueue(ctx, MessageConstants.AUDIT_MODULE_QUEUE);
-        configTopic = JMSUtils.lookupTopic(ctx, MessageConstants.CONFIG_STATUS_TOPIC);
+    	connectionFactory = JMSUtils.lookupConnectionFactory();
+    	responseQueue = JMSUtils.lookupQueue(MessageConstants.COMPONENT_RESPONSE_QUEUE);
+        auditQueue = JMSUtils.lookupQueue(MessageConstants.AUDIT_MODULE_QUEUE);
+        configTopic = JMSUtils.lookupTopic(MessageConstants.CONFIG_STATUS_TOPIC);
     }
 
-    private void lookupConnectionFactory(InitialContext ctx) {
-        LOG.debug("Open connection to JMS broker");
-        try {
-            connectionFactory = (QueueConnectionFactory) ctx.lookup(MessageConstants.CONNECTION_FACTORY);
-        } catch (NamingException ne) {
-            //if we did not find the connection factory we might need to add java:/ at the start
-            LOG.debug("Connection Factory lookup failed for " + MessageConstants.CONNECTION_FACTORY);
-            String wfName = "java:/" + MessageConstants.CONNECTION_FACTORY;
-            try {
-                LOG.debug("trying " + wfName);
-                connectionFactory = (QueueConnectionFactory) ctx.lookup(wfName);
-            } catch (Exception e) {
-                LOG.error("Connection Factory lookup failed for both " + MessageConstants.CONNECTION_FACTORY  + " and " + wfName);
-                throw new RuntimeException(e);
-            }
-        }
-        try {
-            connection = connectionFactory.createConnection();
-            connection.start();
-        } catch (JMSException ex) {
-            LOG.error("Error when open connection to JMS broker");
-        }
-    }
     
     @Override
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public String sendDataSourceMessage(String text, DataSourceQueue queue) throws MessageException {
-        try {
-            connectToJMS();
+    public String sendDataSourceMessage(String text, DataSourceQueue queue) throws MessageException {        
+    	Connection connection=null;
+    	try {
+    		
+    		connection = connectionFactory.createConnection();
+            final Session session = JMSUtils.connectToQueue(connection);
+            
             TextMessage message = session.createTextMessage();
             message.setJMSReplyTo(responseQueue);
             message.setText(text);
@@ -110,13 +86,15 @@ public class MessageProducerBean implements MessageProducer {
             LOG.error("[ Error when sending message. ] {0}", e.getMessage());
             throw new MessageException("[ Error when sending message. ]", e);
         } finally {
-            disconnectFromJMS();
+        	JMSUtils.disconnectQueue(connection);
         }
     }
 
     public void sendConfigDeployedMessage(String text) throws MessageException {
-        try {
-            connectToJMS();
+    	Connection connection=null;
+    	try {
+    		connection = connectionFactory.createConnection();
+            final Session session = JMSUtils.connectToQueue(connection);
             TextMessage message = session.createTextMessage(text);
             getProducer(session, configTopic).send(message);
         }
@@ -125,7 +103,7 @@ public class MessageProducerBean implements MessageProducer {
             throw new MessageException("Error when sending config deployed message.", e);
         }
         finally {
-            disconnectFromJMS();
+        	JMSUtils.disconnectQueue(connection);
         }
     }
 
@@ -148,8 +126,10 @@ public class MessageProducerBean implements MessageProducer {
     }
 
     private void sendReply(TextMessage requestMessage, String response) throws MessageException {
-        try {
-            connectToJMS();
+    	Connection connection=null;
+    	try {
+    		connection = connectionFactory.createConnection();
+            final Session session = JMSUtils.connectToQueue(connection);
             TextMessage responseMessage = session.createTextMessage(response);
             responseMessage.setJMSCorrelationID(requestMessage.getJMSMessageID());
             getProducer(session, requestMessage.getJMSReplyTo()).send(responseMessage);
@@ -159,27 +139,10 @@ public class MessageProducerBean implements MessageProducer {
             throw new MessageException("Error when replying to module message.", e);
         }
         finally {
-            disconnectFromJMS();
+        	JMSUtils.disconnectQueue(connection);
         }        
     }
 
-    private void connectToJMS() throws JMSException {
-        connection = connectionFactory.createConnection();
-        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        connection.start();
-    }
-
-    private void disconnectFromJMS() {
-        try {
-            if (connection != null) {
-                connection.stop();
-                connection.close();
-            }
-        }
-        catch (JMSException e) {
-            LOG.error("[ Error when closing JMS connection ] {}", e.getMessage());
-        }
-    }
 
     private javax.jms.MessageProducer getProducer(Session session, Destination destination) throws JMSException {
         javax.jms.MessageProducer producer = session.createProducer(destination);
