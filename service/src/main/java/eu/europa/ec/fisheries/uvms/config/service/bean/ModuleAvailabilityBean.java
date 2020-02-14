ae77a0ff171a5daff16f4f5e3832471901d25602
Jonas E.Mattsson <jonas.mattsson@consid.se>
2020-02-14 14:27:16 +0100

    removed jackson and placed timestamps as standard.

--- service/src/main/java/eu/europa/ec/fisheries/uvms/config/service/bean/ModuleAvailabilityBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/config/service/bean/ModuleAvailabilityBean.java
@@ -12,7 +12,6 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
 package eu.europa.ec.fisheries.uvms.config.service.bean;
 
 import javax.ejb.Singleton;
-import java.time.Duration;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,7 +19,6 @@ import java.util.Map;
 @Singleton
 public class ModuleAvailabilityBean {
 
-    private static final int FIVE_MINUTES = 300000;
     private Map<String, Instant> timestampByModule;
 
     public ModuleAvailabilityBean() {
@@ -35,8 +33,6 @@ public class ModuleAvailabilityBean {
         return new HashMap<>(timestampByModule);
     }
 
-    public static boolean isOnline(Instant timestamp) {
-        return Duration.between(Instant.now(), timestamp).minusMillis(FIVE_MINUTES).isNegative();
-    }
+
 
 }
